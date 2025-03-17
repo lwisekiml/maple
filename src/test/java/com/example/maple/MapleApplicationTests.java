5dbb6e92db3d3a3960adf580d7e7653985aaab79
wisekim <lwisekiml@naver.com>
2025-03-17 15:33:29 +0900

    기본 세팅

---
+++ src/test/java/com/example/maple/MapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
